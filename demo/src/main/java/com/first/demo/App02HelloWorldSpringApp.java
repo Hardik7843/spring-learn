882e9ccd00696eb691d79d319b32f7de55dbb1cb
Hardik7843 <hardikprajapati7843@gmail.com>
2026-02-22 23:20:52 +0530

    learnt about bean's wiring

--- demo/src/main/java/com/first/demo/App02HelloWorldSpringApp.java
+++ demo/src/main/java/com/first/demo/App02HelloWorldSpringApp.java
@@ -10,6 +10,10 @@ public class App02HelloWorldSpringApp {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("personAddress"));
+        System.out.println(context.getBean(Address.class));
+
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameter"));
     }
 }
--- demo/src/main/java/com/first/demo/HelloWorldConfiguration.java
+++ demo/src/main/java/com/first/demo/HelloWorldConfiguration.java
@@ -1,12 +1,11 @@
 package com.first.demo;
 
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
 
-import java.time.temporal.ValueRange;
 
-record Person(String name, int age) {
+record Person(String name, int age, Address address) {
 }
 
 record Address(String firstLine, String secondLine, int pinCode) {
@@ -27,11 +26,21 @@ public class HelloWorldConfiguration {
 
     @Bean
     Person person() {
-        return new Person("Hardik", 23);
+        return new Person("Hardik", 23, address());
+    }
+
+    @Bean(name = "personAddress")
+    Address address() {
+        return new Address("Malad", "East", 400097);
     }
 
     @Bean
-    Address address() {
-        return new Address("Malad", "East", 400097);
+    Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    Person person3Parameter(String name, int age, Address personAddress) {
+        return new Person(name, age, personAddress);
     }
 }
