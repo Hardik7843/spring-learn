a9259686c5c460617ee851d10a23d745faa6e0e4
Hardik7843 <hardikprajapati7843@gmail.com>
2026-02-24 23:51:20 +0530

    learning spring further

---
+++ demo/src/main/java/com/first/demo/helloworld/App02HelloWorldSpringApp.java
@@ -0,0 +1,23 @@
+package com.first.demo.helloworld;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class App02HelloWorldSpringApp {
+    static void main(String[] args) {
+
+
+        try (var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);) {
+
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            System.out.println(context.getBean("person"));
+            System.out.println(context.getBean("personAddress"));
+            System.out.println(context.getBean(Address.class));
+            System.out.println(context.getBean(Person.class));
+
+            System.out.println(context.getBean("person2MethodCall"));
+            System.out.println(context.getBean("person3Parameter"));
+        }
+    }
+}
