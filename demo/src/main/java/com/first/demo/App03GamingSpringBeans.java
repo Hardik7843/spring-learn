3ceb74da4259bebb35030c0d9f0549c51e91a586
Hardik7843 <hardikprajapati7843@gmail.com>
2026-02-25 23:13:50 +0530

    made mario bean

--- demo/src/main/java/com/first/demo/App03GamingSpringBeans.java
+++ demo/src/main/java/com/first/demo/App03GamingSpringBeans.java
@@ -1,5 +1,6 @@
 package com.first.demo;
 
+import com.first.demo.game.GameRunner;
 import com.first.demo.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,6 +8,8 @@ public class App03GamingSpringBeans {
     static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
             context.getBean(GamingConsole.class).up();
+
+            context.getBean(GameRunner.class).run();
         }
     }
 }
--- demo/src/main/java/com/first/demo/GamingConfiguration.java
+++ demo/src/main/java/com/first/demo/GamingConfiguration.java
@@ -1,14 +1,28 @@
 package com.first.demo;
 
+import com.first.demo.game.GameRunner;
 import com.first.demo.game.GamingConsole;
 import com.first.demo.game.MarioGame;
+import com.first.demo.game.SuperContraGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GamingConfiguration {
     @Bean
+    @Primary
     GamingConsole game() {
         return new MarioGame();
+    }
+
+    @Bean(name = "superContraGame")
+    GamingConsole superContra() {
+        return new SuperContraGame();
+    }
+
+    @Bean
+    GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
     }
 }
