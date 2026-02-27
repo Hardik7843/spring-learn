ccbf3f69d4b6adca7f34c48c6db0414a47b052b3
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-02-27 17:53:51 +0530

    learn diffrent types of injections

--- demo-02/src/main/java/com/first/demo/examples/a1/DepInjectionLauncherApplication.java
+++ demo-02/src/main/java/com/first/demo/examples/a1/DepInjectionLauncherApplication.java
@@ -10,12 +10,30 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass {
-    @Autowired
     Depedenncy1 depedenncy1;
 
 
-    @Autowired
     Dependency2 dependency2;
+
+    //    @Autowired // Auto wiring is optional in Constructor Injection
+    public YourBusinessClass(Depedenncy1 depedenncy1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor Injection - Your BUsiness Class");
+        this.depedenncy1 = depedenncy1;
+        this.dependency2 = dependency2;
+    }
+
+//    @Autowired
+//    public void setDependency1(Depedenncy1 depedenncy1) {
+//        System.out.println("Getter/Setter Injection - Dependency1");
+//        this.depedenncy1 = depedenncy1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Getter/Setter Injection - Dependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     public String toString() {
         return "Using " + depedenncy1 + " and " + dependency2;
