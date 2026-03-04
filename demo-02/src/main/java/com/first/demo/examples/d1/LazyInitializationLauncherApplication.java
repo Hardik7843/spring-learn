0075fc47789625d7c4e1b31c0e539d7b4a914548
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-03-04 18:07:06 +0530

    hii

---
+++ demo-02/src/main/java/com/first/demo/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,47 @@
+package com.first.demo.examples.d1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+@Component
+class ClassA {
+
+    public void doSomething() {
+        System.out.println("Running ClassA");
+    }
+}
+
+@Component
+@Lazy
+class ClassB {
+    private ClassA classA;
+
+    public ClassB(ClassA classA) {
+        System.out.println("Some Initialization");
+        this.classA = classA;
+    }
+
+    public void doSomething() {
+        System.out.println("Running ClassB");
+    }
+}
+
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+
+    static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)) {
+            System.out.println("Initialization of context is completed");
+            context.getBean(ClassB.class).doSomething();
+        }
+
+    }
+}
