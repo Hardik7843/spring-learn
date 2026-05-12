c3adf03d6135bdb3ef6334663fc03af052ec1494
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-05-12 17:50:26 +0530

    init

---
+++ jpa-hibernate/src/main/java/com/spring_boot_learn/jpa_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,11 @@
+package com.spring_boot_learn.jpa_hibernate.course.jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String
+}
