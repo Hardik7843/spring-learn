84cdd8abe7097c9117aaf587e1223f91f5bec8ed
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-05-13 16:45:55 +0530

    feat: implement Course entity and repository with CRUD operations

---
+++ jpa-hibernate/src/main/java/com/spring_boot_learn/jpa_hibernate/course/Course.java
@@ -0,0 +1,50 @@
+package com.spring_boot_learn.jpa_hibernate.course;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course() {
+
+    }
+
+    public Course(long id,String author,  String name) {
+        this.author = author;
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+    }
+
+
+
+}
--- jpa-hibernate/src/main/java/com/spring_boot_learn/jpa_hibernate/course/jdbc/CourseJdbcRepository.java
+++ jpa-hibernate/src/main/java/com/spring_boot_learn/jpa_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,11 +1,49 @@
 package com.spring_boot_learn.jpa_hibernate.course.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.spring_boot_learn.jpa_hibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
+
+    @Autowired
     private JdbcTemplate springJdbcTemplate;
 
-    private static String
+    private  static String INSERT_QUERY = """
+            INSERT INTO course (id, name, author)
+            VALUES
+                (?, ?, ?);
+            """;
+            
+
+    private  static String DELETE_QUERY = """
+            DELETE FROM course WHERE
+                id=?;
+            """; 
+
+
+    private  static String SELECT_QUERY = """
+            SELECT * FROM course WHERE
+                id=?;
+            """; 
+
+
+
+
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteOne(long id){
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course findById(long id) {
+
+        return springJdbcTemplate.queryForObject(SELECT_QUERY,  new BeanPropertyRowMapper<>(Course.class) , id );
+    }
 }
