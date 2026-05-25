2bcf41d35ba86d6e13ed5aac616f98cf1701ae86
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2026-05-25 17:36:09 +0530

    restful-web-services

---
+++ restful-web-services/src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloWorldController {
+    
+    // @RequestMapping( path = "/hello-world", method=RequestMethod.GET)
+    @GetMapping( path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+}
