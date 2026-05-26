8fe3b03d02cc2eebb1ff0bd0913187c8c2a7c64e
Hardik7843 <hardikprajapati7843@gmail.com>
2026-05-26 23:04:15 +0530

    path params

--- restful-web-services/src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,8 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
     }
 
+    @GetMapping(path = "/path-params/{name}")
+    public HelloWorldBean getPathParamName(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, %s", name));
+    }
 }
